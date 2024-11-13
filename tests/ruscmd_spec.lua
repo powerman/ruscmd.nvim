b8a7edd67a34e667015803a32729049437b4a693
Alex Efros <powerman@powerman.name>
2024-11-13 20:29:22 +0200

    chore: cleanup tests

--- tests/ruscmd_spec.lua
+++ tests/ruscmd_spec.lua
@@ -3,11 +3,6 @@ local assert = require 'luassert'
 local child = require('mini.test').new_child_neovim()
 teardown(child.stop)
 
-local function child_feedkeys(keys)
-    keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
-    child.api.nvim_feedkeys(keys, 'tx', false)
-end
-
 local function child_lines()
     return child.api.nvim_buf_get_lines(0, 0, -1, true)
 end
@@ -21,47 +16,47 @@ end)
 
 describe('langmap', function()
     it('without plugin `овв` should do nothing', function()
-        child_feedkeys 'овв'
+        child.type_keys 'овв'
         assert.same(lines, child_lines())
     end)
 
     it('`овв` should work as `jdd`', function()
         child.lua [[ require('ruscmd').setup {} ]]
-        child_feedkeys 'овв'
+        child.type_keys 'овв'
         assert.same({ 'Lorem', 'dolor', 'sit', 'amet' }, child_lines())
     end)
 end)
 
 describe('cabbrev', function()
     it('without plugin `Жив!<CR>Жй<CR>` should do nothing', function()
-        child_feedkeys 'Жив!<CR>'
+        child.type_keys 'Жив!<CR>'
         assert.same(lines, child_lines())
         assert.no_error(function()
-            child_feedkeys 'Жй<CR>'
+            child.type_keys 'Жй<CR>'
         end)
     end)
 
     it('`Жив!<CR>Жй<CR>` should work as `:bd!<CR>:q<CR>`', function()
         child.lua [[ require('ruscmd').setup {} ]]
-        child_feedkeys 'Жив!<CR>'
+        child.type_keys 'Жив!<CR>'
         assert.same({ '' }, child_lines())
         assert.error_matches(function()
-            child_feedkeys 'Жй<CR>'
+            child.type_keys 'Жй<CR>'
         end, 'Invalid channel')
     end)
 
     it('should add new command', function()
         child.lua [[ require('ruscmd').setup {cabbrev={ ['рудз']='help' }} ]]
-        child_feedkeys 'Жрудз<CR>'
+        child.type_keys 'Жрудз<CR>'
         assert.matches('^[*]help[.]txt[*]', child_lines()[1])
     end)
 
     it('should remove default command', function()
         child.lua [[ require('ruscmd').setup {cabbrev={ ['й']='й' }} ]]
-        child_feedkeys 'Жив!<CR>'
+        child.type_keys 'Жив!<CR>'
         assert.same({ '' }, child_lines())
-        assert.no_error(function()
-            child_feedkeys 'Жй<CR>'
-        end)
+        assert.error_match(function()
+            child.type_keys 'Жй<CR>'
+        end, 'E492:')
     end)
 end)
